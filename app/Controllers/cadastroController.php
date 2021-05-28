6b91cb3a72e31811f9e594986c13dada71b62dbd
CaioFreitas96 <rlcmcaio@gmail.com>
2021-05-27 23:20:10 -0300

    Atualização do cadastro

--- app/Controllers/cadastroController.php
+++ app/Controllers/cadastroController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use Core\Controller;
 use App\Models\Cadastro;
 // Esse use serve para você poder usar o Request, que abstrai tudo relacionado a requisição
@@ -11,32 +12,60 @@ class CadastroController extends Controller{
     
     public function index(Request $request){
         if($request->isMethod('get')){
-            $this->view('cadastro');
+            $this->view('cadastro', ['get' => 'get']);
         }else{
             $cadastroModel = new Cadastro();
             
-         
-           $email2 = ['email' => $request->post('email')];
-           $emailDuplicado = $cadastroModel->getEmail($email2);
+           // EMAIL
+           $email = ['email' => $request->post('email')];
+           $emailDuplicado = $cadastroModel->getEmail($email);
            
+           //NOME
+           $nome = $request->post('nome');
+           
+           //CPF
+           $cpf = $request->post('cpf');
+           $validaCPF = $cadastroModel->validaCPF($cpf);
+           
+           
+           //TEL
+           $tel = $request->post('telefone');
+           $validaTelefone = $cadastroModel->validaTelefone($tel);
+           
+           //EMAIL
+           $filter = filter_var($request->post('email'), FILTER_VALIDATE_EMAIL);
+           $email = ['email' => $request->post('email')];
+           $emailDuplicado = $cadastroModel->getEmail($email);
+           
+           //SENHA
+           $senha = $request->post('senha');
+           $strlen = strlen($senha);
 
-            $validacaoNome = $request->post('nome');
-            $testeEmail = filter_var($request->post('email'), FILTER_VALIDATE_EMAIL);
-            $validacaoSenha = $request->post('senha');
-            
-            if($testeEmail == false || $validacaoNome == null 
-            || $validacaoSenha == null || !empty($emailDuplicado)){
-
-                //$email = " * E-mail invalido";
-                $this->view('cadastro', ['emailDuplicado' => $emailDuplicado, 'email' => $request->post('email'), 'emailfalho' => '* E-mail invalido',
-                 'nome' =>  $request->post('nome'), 'testeEmail' => $testeEmail, 'erroNome' => "* Nome vazio",
-                  'validacaoNome' => $validacaoNome, 'erroSenha' => "* Senha vazia" ,'validacaoSenha' => $validacaoSenha]);
-            }else{
-                $resposta = $cadastroModel->inserir($request->post());
-
+           //DADOS ENVIADOS POST
+           $dados = $request->post(); 
                 
-
-                $this->view('cadastroRealizado', ['resposta' => $resposta, 'emailDuplicado' => $emailDuplicado]);
+            
+            if($nome == null ){
+                $this->view('cadastro', ['nome' => $nome, 'dados' => $dados]);
+            }else if($validaCPF === false){
+                $this->view('cadastro', ['cpf' => $cpf, 'dados' => $dados]);
+            }else if($validaTelefone === false){
+                $this->view('cadastro', ['tel' => $tel, 'dados' => $dados]);
+            }else if($filter === false || !empty($emailDuplicado)){
+                if($filter === false){
+                    $this->view('cadastro', ['email' => $email, 'dados' => $dados]);
+                }else{
+                    $this->view('cadastro', ['emailDuplicado' => $emailDuplicado, 'dados' => $dados]);
+                }
+            }else if($senha == null || $strlen < 8 ){
+                $this->view('cadastro', ['senha' => $senha, 'dados' => $dados]);
+            }else{
+                
+                $inserir = $cadastroModel->inserir($request->post());
+                $dados = $request->post();
+                                
+                unset($dados['cpf']);
+                $this->view('cadastroRealizado', ['dados' => $dados] );
             }
         }
        
--- app/Models/Cadastro.php
+++ app/Models/Cadastro.php
@@ -42,18 +42,46 @@ class Cadastro{
             }
         }
     }
-    public function atualizar($dados, $condicao){
-        $db = Database::getInstance();
-         
-        return $db->update($this->table, $dados, $condicao );
+    
+    function validaCPF($cpf) {
+
+        // Extrai somente os números
+        $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+    
+        // Verifica se foi informado todos os digitos corretamente
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+    
+        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+    
+        // Faz o calculo para validar o CPF
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+        return true;
+    
     }
-    // essa condição significa se não passa nada, ele não altera nada.
-    public function deletar($id = null){
-        $db = Database::getInstance();
-        if($id != null && !empty($id)){
-            $condicao = ["id" => $id];
-            return $db->delete($this->table, $condicao);
-        }    
-        return false;
+    function validaTelefone($tel){
+        $numero = is_numeric($tel);
+        if($numero == true){
+            if(strlen($tel) ==11){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
+
 }
